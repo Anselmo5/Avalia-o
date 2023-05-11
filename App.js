98705ebe3a47ab1abe9d2d672d461b16439ef7bd
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-05-11 08:41:04 -0300

    Update App.js

--- App.js
+++ App.js
@@ -34,14 +34,7 @@ function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.closeDrawer()}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.toggleDrawer()}
-      />
+    
     </DrawerContentScrollView>
   );
 }
